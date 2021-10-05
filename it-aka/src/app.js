9f51aa77e01e550001067dc172b5d7f562570363
Beatrizfleon <Beatrizfleon@gmail.com>
2021-10-05 16:38:43 +0200

    route get-db-collection

--- it-aka/src/app.js
+++ it-aka/src/app.js
@@ -1,10 +1,14 @@
 const Koa = require('koa')
 
+const axios = require('axios');
+
 // acceder aux params depuis le contexte
 const koaBody = require('koa-body')
 
 // declarer un router pour avoir plusieurs route
 const Router = require('koa-router')
+
+
 
 
 // Declare Main Application
@@ -32,6 +36,122 @@ router.get('/hola', (ctx, next) => {
     'name': 'Pepe',
     'age': 25
   }
+})
+router.post('/body-parser-object/', (ctx, next) => {
+    ctx.body = ctx.request.body
+})
+
+router.put('/body-parser-object/:id', (ctx, next) => {
+    ctx.request.body.id = ctx.params.id
+    ctx.body = ctx.request.body
+})
+
+router.post('/body-parser-string/', (ctx, next) => {
+    ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
+})
+
+router.post('/authentification-to-db',  async (ctx, next) => {
+  // a vous de coder
+  // return jwt
+  console.log(ctx)
+  // on recuperer les elements distincts
+  // ctx.request.body = { identifier: 'itakad@gmail.com', password: 'itakad2020' }
+  let login = ctx.query.login
+  let password = ctx.query.password
+
+  // declare une variable pour le json web token
+  var jwt = null
+
+  // si les login et password sont correct je recupere un jwt
+  await axios.post('https://gql.alcyone.life/auth/local', {
+      identifier: login,
+      password: password,
+  })
+  .then(response =>{
+      console.log(response.data) // handle succes
+      // extraction du jwt de la reponse
+      jwt = response.data.jwt
+  })
+  .catch(function (error) {
+      console.log('error') // handle error
+  })
+  .then(function () {
+      console.log('--- --- ---')
+  });
+  // affichage jwt
+  console.log(jwt)
+  // je retourne un object avec le jwt
+  ctx.body = {jwt: jwt}
+
+
+})
+
+
+// with query string
+router.get('/authentification-to-db',  async (ctx, next) => {
+
+  console.log(ctx)
+  // on recuperer les elements distincts
+  // ctx.request.body = { identifier: 'itakad@gmail.com', password: 'itakad2020' }
+  let login = ctx.query.login
+  let password = ctx.query.password
+
+  // declare une variable pour le json web token
+  var jwt = null
+
+  // si les login et password sont correct je recupere un jwt
+  await axios.post('https://gql.alcyone.life/auth/local', {
+      identifier: login,
+      password: password,
+  })
+  .then(response =>{
+      // extraction du jwt de la reponse
+      jwt = response.data.jwt
+  })
+  .catch(function (error) {
+      console.log('error') // handle error
+  })
+  .then(function () {
+      console.log('--- --- ---')
+  });
+  // affichage jwt
+  console.log(jwt)
+  // je retourne un object avec le jwt
+  ctx.body = {jwt: jwt}
+})
+
+
+
+router.get('/get-db-collection/:collection', async (ctx, next) => {
+  // recuperer en parametre le nom de la collection a requeter
+  let collection = ctx.params.collection
+  let ret = null
+  // a vous de coder
+  // return content of collection
+
+    // ensuite requeter la base de donnee
+    await axios.get(`https://gql.alcyone.life/${collection}`)
+    .then(response => {
+        ret = response.data
+    })
+    .catch(function (error) {
+        console.log('error') // handle error
+    })
+    .then(function () {
+        console.log('--- --- ---')
+    });
+
+    // a vous de coder
+    // return content of collection
+    ctx.body = ret
+
+
+})
+
+
+router.post('/',  (ctx, next) => {
+  // a vous de coder
+  return {jwt: jwt}
 })
 
 
--- it-aka/src/client.js
+++ it-aka/src/client.js
@@ -2,13 +2,89 @@
    
 const axios = require('axios');
 
-// get hello-world
-axios.get('http://localhost:3000/hola')
-  .then(function (response) {
-    console.log(response.data) // handle succes
 
-    //la reponse c'est ce qu'elle return la arrow fonction 
+//authentification-to-db
+
+// // parse object and convert to string
+// axios.get('http://localhost:3000/authentification-to-db', {login: 'itakad@gmail.com', password: 'itakad2020' })
+//   .then(function (response) {// handle succes
+//     console.log(response.data);
+//   })
+//   .catch(function (error) { // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     console.log('--- --- ---')
+//   });
+
+
+  axios.get('http://localhost:3000/get-db-collection/categories')
+  .then(function (response) {// handle succes
+    console.log(response.data);
   })
-  .catch(function (error) {
-    console.log('error') // handle error
+  .catch(function (error) { // handle error
+    console.log(error);
+  })
+  .then(function () {
+    console.log('--- --- ---')
   });
+
+
+
+
+
+
+// // get hello-world
+// axios.get('http://localhost:3000/hola')
+//   .then(function (response) {
+//     console.log(response.data) // handle succes
+
+//     //la reponse c'est celle que return la arrow fonction 
+//   })
+//   .catch(function (error) {
+//     console.log('error') // handle error
+//   })
+//   .then(function () {
+//     console.log('--- --- ---')
+//   });
+
+
+
+// // parse object
+// axios.post('http://localhost:3000/body-parser-object/', {login: 'test', password: 'pwd' })
+// .then(function (response) { 
+//   console.log(response.data) // handle succes
+// })
+// .catch(function (error) { 
+//   console.log(error) // handle error
+// })
+// .then(function () {
+//   console.log('--- --- ---')
+// });
+
+
+
+// // parse object and add params :id
+// axios.put('http://localhost:3000/body-parser-object/1', {login: 'test', password: 'pwd' })
+// .then(function (response) { 
+// console.log(response.data) // handle succes
+// })
+// .catch(function (error) { 
+// console.log(error) // handle error
+// })
+// .then(function () {
+// console.log('--- --- ---')
+// });
+
+
+// // parse object and convert to string
+// axios.post('http://localhost:3000/body-parser-string/', {login: 'test', password: 'pwd' })
+// .then(function (response) {// handle succes
+// console.log(response.data);
+// })
+// .catch(function (error) { // handle error
+// console.log(error);
+// })
+// .then(function () {
+// console.log('--- --- ---')
+// });
